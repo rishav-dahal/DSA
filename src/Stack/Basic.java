90b264deb8da69c722d159b11e2520ce48a1c26b
Rishav Dahal <82385662+rishav-dahal@users.noreply.github.com>
2023-01-24 09:14:47 +0545

    Day 1

---
+++ src/Stack/Basic.java
@@ -0,0 +1,27 @@
+package Stack;
+import java.util.Stack;
+public class Basic {
+    public static void main(String[]args){
+        //Using stack class and using string data type to make a stack object
+        Stack<String> stack = new Stack<>();
+
+        /*Stack class has push() pop() empty() etc methods
+        more at https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html
+         */
+        System.out.println(stack.empty());
+
+        stack.push("Rishav");
+        stack.push("Shrayash");
+        stack.push("Jenisha");
+        stack.push("Sandhya");
+
+        System.out.println(stack.empty());
+        System.out.println(stack);
+
+        String pop= stack.pop();
+        System.out.println(stack);
+        System.out.println(pop);
+
+
+    }
+}
